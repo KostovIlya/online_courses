fe89c3d1398e47afe26dd92996a720ef7cc627a9
Kostov Ilya <kick1991@mail.ru>
2023-10-23 18:48:27 +0500

    add models user, course, lesson

---
+++ courses/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from users.models import NULLABLE
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    preview = models.ImageField(upload_to='courses/preview', verbose_name='превью(картинка)', **NULLABLE)
+    description = models.TextField(verbose_name='описание')
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    preview = models.ImageField(upload_to='courses/preview', verbose_name='превью(картинка)', **NULLABLE)
+    video_link = models.URLField(verbose_name='ссылка на видео')
